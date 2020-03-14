c8f457df19504decd2c3bda1805c9f43e8d08eba
erkangz <erkang.zheng@lifeomic.com>
2020-03-13 23:43:39 -0400

    adjust query; update comment; add screenshot

--- src/set-user-sso-mfa.js
+++ src/set-user-sso-mfa.js
@@ -13,22 +13,31 @@ const {
 } = process.env;
 
 /**
+ * Name of the SSO provider in all lowercase. Replace as appropriate.
+ */
+const ssoProvider = 'okta';
+
+/**
  * The starting query to find a set of entities to enrich
  */
-const baseQuery = `Find User with _integrationClass!='SSO'`;
+const baseQuery = `Find User with _type!='${ssoProvider}_user'`;
 
 /**
  * A collection of enrichment rules. 
- * Based on the query result of each rule, set properties on match or noMatch.
+ *
+ * Based on the query result of each rule, set certain properties when a
+ * matching result is found or not found.
  */
 const enrichmentRules = [
   {
-    query: `Find * with _key='{{entityKey}}' as userA 
+    query: `Find User with _key='{{entityKey}}' as userA 
 that has Account 
-  that connects Application with _integrationClass='SSO'
-  that ASSIGNED User as userB
+  that connects ${ssoProvider}_application
+  that assigned ${ssoProvider}_user as userB
 where 
-  userA.name = userB.name or userA.username = userB.username`,
+  userA.name = userB.name or 
+  userA.username = userB.username or
+  userA.email = userB.email`,
     whenFound: {
       ssoUser: true
     },
@@ -37,13 +46,15 @@ where
     },
   },
   {
-    query: `Find * with _key='{{entityKey}}' and ssoUser=true as userA 
+    query: `Find User with _key='{{entityKey}}' and ssoUser=true as userA 
   that has Account 
-  that connects Application with _integrationClass='SSO'
-  that assigned User as userB
-  that (assigned|has) mfa_device
+  that connects ${ssoProvider}_application
+  that assigned ${ssoProvider}_user as userB
+  that (assigned|has|uses) mfa_device
 where 
-  userA.name = userB.name or userA.username = userB.username`,
+  userA.name = userB.name or 
+  userA.username = userB.username or
+  userA.email = userB.email`,
     whenFound: {
       mfaEnabled: true
     },
